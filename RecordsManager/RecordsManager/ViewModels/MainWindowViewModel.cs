fc09abb518b1be1fe63a6bc1f7348d98ce09b477
VadimKostin <vadkostinwork@gmail.com>
2023-08-25 10:49:56 +0300

    Add MainWindowViewModel

---
+++ RecordsManager/RecordsManager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using RecordsManager.Models;
+using RecordsManager.ServiceContracts;
+using RecordsManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordsManager.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        #region SERVICES
+        private readonly IRecordsService _recordsService;
+        private readonly IExpensesService _expensesService;
+        #endregion
+
+        #region BINDING PROPERTIES
+        public List<RecordGroupedModel> Records { get; set; }
+        public List<ExpenseGroupedModel> Expenses { get; set; }
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            _recordsService = new RecordsService();
+            _expensesService = new ExpensesService();
+
+            Records = _recordsService.GetAllGroupedAsync().GetAwaiter().GetResult();
+            Expenses = _expensesService.GetAllGroupedAsync().GetAwaiter().GetResult();
+        }
+    }
+}
